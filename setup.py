d8fa92808e749f607a8b05011028a95a06b44d5d
Philippe Chavanne <philippe.chavanne@gmail.com>
2016-01-25 14:24:53 +0100

    dl first commit

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
+from setuptools import find_packages
 from setuptools import setup
 
 setup(name="dl",
-      version="0.0.0",
-      description="Personal deep learning framework for Theano"
-                  "This is highly inspired by Lasagne. I highly recommend Lasagne"
-                  "instead of dl",
-      classifiers=["Development Status :: 3 - Alpha",
-                   "Intended Audience :: Developers",
-                   "Intended Audience :: Science/Research",
-                   "License :: OSI Approved :: MIT License",
-                   "Programming Language :: Python :: 2.7",
-                   "Topic :: Scientific/Engineering :: Artificial Intelligence",
-                  ],
-      keywords="",
+      version="0.0.1",
+      description="Deep learning ultra light framework based on Theano",
       author="Philippe Chavanne",
       author_email="philippe.chavanne@gmail.com",
-      license="MIT"
+      url="https://github.com/pchavanne/dl",
+      license="MIT",
+      install_requires=['numpy', 'theano'],
+      packages=find_packages()
       )
