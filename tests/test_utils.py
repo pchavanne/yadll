ff3d8c60c9125df6a40cfec326f84e42db934fc3
Philippe Chavanne <philippe.chavanne@gmail.com>
2016-01-28 09:42:36 +0100

    dl first commit

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,16 @@
-#!/usr/bin/env python
 # -*- coding: UTF-8 -*-
+import numpy as np
 
-__author__ = "Philippe Chavanne"
+import theano
+
+import dl
+
+
+def test_to_float_X():
+    x = np.asarray(np.random.normal(size=(10,5)))
+    assert dl.utils.to_float_X(x).dtype == theano.config.floatX
+
+
+def test_shared_variable():
+    x = np.asarray(np.random.normal(size=(10,5)))
+    assert isinstance(dl.utils.shared_variable(x), theano.compile.SharedVariable)
