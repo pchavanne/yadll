e94f244b58d325e9d388beb8e9fce5bede81d238
Philippe Chavanne <philippe.chavanne@gmail.com>
2016-06-20 11:11:09 +0200

    first yadll commit

--- yadll/network.py
+++ yadll/network.py
@@ -3,6 +3,23 @@ from .layers import *
 
 
 class Network(object):
+    """
+    The :class:`Network` class is the container of all the layers of the network.
+
+    Parameters
+    ----------
+    name : `string`
+        The name of the network
+    layers : list of :class: `Layer`, optional
+        create a network from another network.layers
+
+    Attributes
+    ----------
+    layers : list of :class: `Layer`
+        the list of layers in the network
+    params : list of `theano shared variables`
+        the list of all the parameters of the network
+    """
     def __init__(self, name=None, layers=None):
         self.layers = []
         self.params = []
@@ -14,6 +31,14 @@ class Network(object):
                 self.add(layer)
 
     def add(self, layer):
+        """
+        add a layer to the Network
+
+        Parameters
+        ----------
+        layer: :class: `Layer`
+
+        """
         self.layers.append(layer)
         self.params.extend(layer.params)
         self.reguls += layer.reguls
@@ -21,10 +46,37 @@ class Network(object):
             self.has_unsupervised_layer = True
 
     def params(self):
+        """
+        Returns the list of parameters of the network
+
+        Returns
+        -------
+        params:
+            list of parameters of the network
+
+        """
         return self.params
 
     def reguls(self):
+        """
+        Returns the regularization cost for the network
+
+        Returns
+        -------
+        reguls: symbolic expresssion
+            regularization cost for the network
+
+        """
         return self.reguls
 
     def get_output(self, **kwargs):
+        """
+        Returns the output of the network
+
+        Returns
+        -------
+        symbolic expresssion
+            output of the network
+
+        """
         return self.layers[-1].get_output(**kwargs)
