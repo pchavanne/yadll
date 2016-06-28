3d5682409c069400ddd64084aa432e473792ecd3
Philippe Chavanne <philippe.chavanne@gmail.com>
2016-06-28 13:17:40 +0200

    first yadll commit

--- yadll/objectives.py
+++ yadll/objectives.py
@@ -52,7 +52,7 @@ def mean_absolute_error(prediction, target):
     """
     Mean Absolute Error
 
-    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\\abs{target_{i,j} - predicition_{i,j}}}
+    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\\norm{target_{i,j} - predicition_{i,j}}}
 
     Parameters
     ----------
@@ -72,7 +72,7 @@ def hinge(prediction, target):
     """
     Hinge Error
 
-    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\\abs{target_{i,j} - predicition_{i,j}}}
+    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\\norm{target_{i,j} - predicition_{i,j}}}
 
     Parameters
     ----------
@@ -89,11 +89,43 @@ def hinge(prediction, target):
 
 
 def binary_crossentropy(prediction, target):
+    """
+    Binary Crossentropy Error
+
+    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\abs{target_{i,j} - predicition_{i,j}}}
+
+    Parameters
+    ----------
+    prediction : Theano tensor
+        The predicted values
+    target : Theano tensor
+        The target values
+
+    Returns
+    -------
+        Binary crossentropy
+    """
     clip_pred = T.clip(prediction, EPSILON, 1 - EPSILON)
     return T.mean(T.nnet.binary_crossentropy(clip_pred, target), axis=-1)
 
 
 def categorical_crossentropy(prediction, target):
+    """
+    Categorical Crossentropy Error
+
+    .. math:: MAE_i = \\frac{1}{n} \\sum_{j}{\abs{target_{i,j} - predicition_{i,j}}}
+
+    Parameters
+    ----------
+    prediction : Theano tensor
+        The predicted values
+    target : Theano tensor
+        The target values
+
+    Returns
+    -------
+        Binary crossentropy
+    """
     prediction /= prediction.sum(axis=-1, keepdims=True)
     prediction = T.clip(prediction, EPSILON, 1 - EPSILON)
     return T.mean(T.nnet.categorical_crossentropy(prediction, target), axis=-1)
